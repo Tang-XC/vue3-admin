4103f2007a6e5bd7a4bdf58d3c940402adaa67a4
tangxiaochao <Tang-XC@outlook.com>
2024-04-23 10:25:05 +0800

    feat(custom): 增加了husky + commitlint进行提交描述检查

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      //当前验证的错误级别
+      2,
+      //在什么情况下进行验证
+      'always',
+      //泛型内容
+      [
+        'feat', //新功能feature
+        'fix', //修复bug
+        'docs', //文档注释
+        'style', //代码格式(不影响代码运行的变动)
+        'refactor', //重构(既不增加新功能，也不是修复bug)
+        'perf', //性能优化
+        'test', //增加测试
+        'chore', //重构过程或辅助工具的变动
+        'revert', //回退
+        'build' //打包
+      ]
+    ],
+    //subject 大小不做校验
+    'subject-case': [0]
+  }
+}
