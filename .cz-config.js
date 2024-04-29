779a8761fb3248f07737cd520f318aabf2f8fa7f
tangxiaochao <Tang-XC@outlook.com>
2024-04-29 11:32:22 +0800

    chore: 修改cz

--- .cz-config.js
+++ .cz-config.js
@@ -18,7 +18,7 @@ module.exports = {
   // 消息步骤
   messages: {
     type: '请选择提交类型:',
-    // customScope: '请输入修改范围(可选):',
+    scope: '请选择修改范围(可选):',
     subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选):',
     footer: '请输入要关闭的issue(可选):',
@@ -28,7 +28,7 @@ module.exports = {
 
   scope: { empty: true },
   // 跳过问题
-  skipQuestions: ['body', 'footer'],
+  skipQuestions: ['scope', 'body', 'footer'],
   // subject文字长度默认是72
   subjectLimit: 72
 }
